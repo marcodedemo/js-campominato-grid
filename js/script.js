acf7611ce1312fc9bfe63a5f7ddca09700b556e6
marcodedemo <marco.dedemo@gmail.com>
2023-03-03 13:35:23 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+// al click del pulsante "genera" viene generata una griglia di 10*10 quadrati
+// inserisco nelle celle i numeri progressivi da 1 a 100
+
+// al click di una cella quest'ultima si colora di azzurro e stampa in console il numero inserito precedentemente dentro la cella
+
